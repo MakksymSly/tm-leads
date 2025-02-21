23b394b881cdcdab1513f6fbe88f3b66a1f20da5
Maksym Seliutin <maxwell7k@gmail.com>
2025-02-21 22:19:39 +0200

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.scss'
+import { Header } from './Components/Header/Header'
+
+function App() {
+
+  return (
+    <>
+      <section>
+        <Header />
+        </section>
+        
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Header/Header.tsx
@@ -0,0 +1,8 @@
+import './Header.scss'
+export const Header = () => {
+  return (
+    <header className='header'>
+      <a href='#'><img src="/logo.svg" alt="logo" /></a>
+    </header>
+  )
+}
